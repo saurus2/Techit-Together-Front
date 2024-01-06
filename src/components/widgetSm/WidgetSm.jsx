6cb179240897f624312d081e36fe95c741026fea
Jihyeok <bluesaurus2@gmail.com>
2024-01-06 14:37:43 -0800

    Title: Editing Small Widget     Contents: User member list     Comments:

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import './widgetSm.css'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+export default function WidgetSm() {
+    return <div className="widgetSm">
+        <span className="widgetSmTitle">New Join Member</span>
+        <ul className="widgetSmList">
+            <li className="widgetSmListItem">
+                <AccountCircleIcon className="widgetSmImg"/>
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername">Yolo Kim</span>
+                    <span className="widgetSmUserTitle">SWE</span>
+                </div>
+                <button className="widgetSmButton">
+                    <VisibilityIcon className="widgetSmIcon"/>
+                    Display
+                </button>
+            </li>
+            <li className="widgetSmListItem">
+                <AccountCircleIcon className="widgetSmImg"/>
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername">Yolo Lee</span>
+                    <span className="widgetSmUserTitle">SWE</span>
+                </div>
+                <button className="widgetSmButton">
+                    <VisibilityIcon className="widgetSmIcon"/>
+                    Display
+                </button>
+            </li>
+            <li className="widgetSmListItem">
+                <AccountCircleIcon className="widgetSmImg"/>
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername">Yolo Park</span>
+                    <span className="widgetSmUserTitle">SWE</span>
+                </div>
+                <button className="widgetSmButton">
+                    <VisibilityIcon className="widgetSmIcon"/>
+                    Display
+                </button>
+            </li>
+        </ul>
+    </div>
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featureInfo/Featuredinfo'
 import './home.css'
 import { userData } from '../../dummyData'
+import WidgetSm from '../../components/widgetSm/WidgetSm'
+import WidgetLg from '../../components/widgetLg/WidgetLg'
 
 export default function Home() {
     return <div className="home">
@@ -13,5 +15,9 @@ export default function Home() {
             grid
             dataKey="Active User"
         />
+        <div className='homeWidgets'>
+            <WidgetSm />
+            <WidgetLg />
+        </div>
     </div>
 }
