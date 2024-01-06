a51d237651c20917a092c1557265d700136a2f90
Jihyeok <bluesaurus2@gmail.com>
2024-01-06 12:13:35 -0800

    Title: Update Contents: Sidebar items Comments: None

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './sidebar.css'
+// https://mui.com/ icon address website
 import HomeWorkIcon from '@mui/icons-material/HomeWork'
 import TrendingUpIcon from '@mui/icons-material/TrendingUp'
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn'
+import PersonOutlineIcon from '@mui/icons-material/Group';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import LocalAtmIcon from '@mui/icons-material/Paid';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import EmailIcon from '@mui/icons-material/Email';
+import FeedbackIcon from '@mui/icons-material/Feedback';
+import MessagesIcon from '@mui/icons-material/Message';
+import ManageIcon from '@mui/icons-material/Work';
+import AnalyticsIcon from '@mui/icons-material/Timeline';
+import ReportsIcon from '@mui/icons-material/ReportGmailerrorred';
 
 export default function sidebar() {
     return (
@@ -22,6 +33,61 @@ export default function sidebar() {
                         <li className="sidebarListItem">
                             <MonetizationOnIcon />
                             Sales
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Quick</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <PersonOutlineIcon className='sidebarIcon'/>
+                            Users
+                        </li>
+                        <li className="sidebarListItem">
+                            <StorefrontIcon className='sidebarIcon'/>
+                            Products
+                        </li>
+                        <li className="sidebarListItem">
+                            <LocalAtmIcon className='sidebarIcon'/>
+                            Transactions
+                        </li>
+                        <li className="sidebarListItem">
+                            <BarChartIcon className='sidebarIcon'/>
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Notification</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <EmailIcon className='sidebarIcon'/>
+                            Email
+                        </li>
+                        <li className="sidebarListItem">
+                            <FeedbackIcon className='sidebarIcon'/>
+                            Feedback
+                        </li>
+                        <li className="sidebarListItem">
+                            <MessagesIcon className='sidebarIcon'/>
+                            Messages
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">staff</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <ManageIcon className='sidebarIcon'/>
+                            Manage
+                        </li>
+                        <li className="sidebarListItem">
+                            <AnalyticsIcon className='sidebarIcon'/>
+                            Analytics
+                        </li>
+                        <li className="sidebarListItem">
+                            <ReportsIcon className='sidebarIcon'/>
+                            Reports
                         </li>
                     </ul>
                 </div>
