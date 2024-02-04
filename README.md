eefb7a8256ca64a4750b462ea523cc5c46bf054c
Chaejun Lee <chaejunlee@knu.ac.kr>
2024-02-03 18:32:57 -0800

    chore: add prettier

--- README.md
+++ README.md
@@ -7,15 +7,18 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-
 ## Install
+
 Vite Installation Commend: npm install vite
 Router Installation Commend: npm install react-router-dom --save
 Rechart Installatiown Commend: npm install recharts
 
 ## Commands
+
 Install vite
+
 - npm i
 
 Running
+
 - npm run dev
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
-import Chart from '../../components/chart/Chart';
-import FeaturedInfo from '../../components/featureInfo/Featuredinfo';
-import './home.css';
-import { userData } from '../../dummyData';
-import WidgetSm from '../../components/widgetSm/WidgetSm';
-import WidgetLg from '../../components/widgetLg/WidgetLg';
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featureInfo/Featuredinfo";
+import "./home.css";
+import { userData } from "../../dummyData";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 
 export default function Home() {
   return (
