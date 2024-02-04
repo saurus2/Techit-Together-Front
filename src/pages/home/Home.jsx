5a8756a5c2d53d965b8e18e0221b0c34126ad46f
dasom <dasomlee923@gmail.com>
2024-02-03 19:43:34 -0800

    top community updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,11 +4,13 @@ import './home.css';
 import { userData } from '../../dummyData';
 import WidgetSm from '../../components/widgetSm/WidgetSm';
 import WidgetLg from '../../components/widgetLg/WidgetLg';
+import Top_Community from '../../components/top_community/Top_Community';
 
 export default function Home() {
   return (
     <div className="home">
-      <FeaturedInfo />
+{/*       <FeaturedInfo /> */}
+    <Top_Community />
       <Chart
         data={userData}
         title="User Analytics"
