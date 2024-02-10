ff6af073f34900af910d1b5200e17aeb6501eaee
Jihyeok <bluesaurus2@gmail.com>
2024-02-10 13:21:13 -0800

    Title: update chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,11 +4,13 @@ import './home.css';
 import { userData } from '../../dummyData';
 import WidgetSm from '../../components/widgetSm/WidgetSm';
 import WidgetLg from '../../components/widgetLg/WidgetLg';
+import CircleChart from '../../components/circleChart/CircleChart';
 
 export default function Home() {
   return (
     <div className="home">
-      <FeaturedInfo />
+      <CircleChart />
+      {/* <FeaturedInfo /> */}
       <Chart
         data={userData}
         title="User Analytics"
